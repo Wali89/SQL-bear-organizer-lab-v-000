8176e6a16104e945ebb025edcc5aab5151021282
Wali Rahman <wali.rahman89@yahoo.com>
2018-03-04 22:45:33 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,7 @@
-1.	sqlite> INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("column_1 data", 3, " column_3 data");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Brown", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Black", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Brown", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "M", "White", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 0, "M", "Brown", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "M", "Brown", 1);
